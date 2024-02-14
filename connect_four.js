7c7a4ce48763be56897ae8ec21e14cee5a967f75
jcwaldron <125517332+jcwaldron@users.noreply.github.com>
2024-02-14 13:30:39 -0600

    set up files to begin work on game itself

--- connect_four.js
+++ connect_four.js
@@ -1,1 +1,13 @@
-// placeholder-2
+let containerId = "connect-four";
+
+function loadConnectFour(containerId) {
+    let container = document.getElementById(containerId);
+    let board = document.createElement("div");
+    board.className = "connect-four-board";
+    board.textContent = "test";
+
+
+    container.appendChild(board);
+    return board;
+    
+}
