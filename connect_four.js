d1f8d56ffa17bf694c779f03fac627a02cbc770a
jcwaldron <125517332+jcwaldron@users.noreply.github.com>
2024-02-14 14:11:38 -0600

    visual elements complete so I can see what I'm doing

--- connect_four.js
+++ connect_four.js
@@ -4,10 +4,26 @@ function loadConnectFour(containerId) {
     let container = document.getElementById(containerId);
     let board = document.createElement("div");
     board.className = "connect-four-board";
-    board.textContent = "test";
-
-
     container.appendChild(board);
-    return board;
-    
+    loadBoard(board); 
 }
+
+function loadBoard(board) {
+    // Create the empty game board HTML structure
+    for (let i = 0; i < 6; i++) {
+        let rowDiv = document.createElement("div");
+        rowDiv.className = "connect-four-row";
+        for (let j = 0; j < 7; j++) {
+            let cellDiv = document.createElement("div");
+            cellDiv.className = "connect-four-cell";
+            cellDiv.innerText="x";
+            rowDiv.appendChild(cellDiv);
+        }
+        board.appendChild(rowDiv);
+    }
+}
+
+// Call the loadConnectFour function when the page loads
+window.onload = function() {
+    loadConnectFour(containerId);
+};
